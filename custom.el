23edda93fb44e3c2a5a6ed263e4008e158eb3ac7
agroschim <agroschim@gmx.de>
2016-10-22 23:29:00 +0200

    Installed diff-hl and magit

--- custom.el
+++ custom.el
@@ -6,7 +6,7 @@
  '(inhibit-startup-screen t)
  '(package-selected-packages
    (quote
-    (hy-mode clojure-mode auto-complete yasnippet tree-mode sunrise-commander rainbow-mode powerline pdf-tools ox-mediawiki neotree monokai-theme json-mode iedit flycheck expand-region dired-subtree csharp-mode auto-complete-c-headers auto-complete-auctex))))
+    (magit diff-hl hy-mode clojure-mode auto-complete yasnippet tree-mode sunrise-commander rainbow-mode powerline pdf-tools ox-mediawiki neotree monokai-theme json-mode iedit flycheck expand-region dired-subtree csharp-mode auto-complete-c-headers auto-complete-auctex))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
