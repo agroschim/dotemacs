813ba40fbc3931f0ab5227ef6802a364e792b1b9
agroschim <agroschim@gmx.de>
2016-11-15 19:59:54 +0100

    Set all-the-icons-dired-dir-face to Monokai blue

--- custom.el
+++ custom.el
@@ -12,4 +12,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(all-the-icons-dired-dir-face ((t (:foreground "#66D9EF")))))
