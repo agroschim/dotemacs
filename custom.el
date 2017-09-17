c6fbc27afede6298571c5fa93cafdd6420ecb207
agroschim <agroschim@gmx.de>
2017-09-17 19:01:52 +0200

    Deactivated some elpy modules

--- custom.el
+++ custom.el
@@ -3,6 +3,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(elpy-modules
+   (quote
+    (elpy-module-company elpy-module-eldoc elpy-module-pyvenv elpy-module-yasnippet elpy-module-sane-defaults)))
  '(inhibit-startup-screen t)
  '(package-selected-packages
    (quote
