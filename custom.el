50b53268e43b9299ef6720693771af36e7311fbc
agroschim <agroschim@gmx.de>
2018-02-19 20:42:24 +0100

    Removed some more packages in connection with elpy

--- custom.el
+++ custom.el
@@ -3,9 +3,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(elpy-modules
-   (quote
-    (elpy-module-company elpy-module-eldoc elpy-module-pyvenv elpy-module-yasnippet elpy-module-sane-defaults)))
  '(inhibit-startup-screen t)
  '(org-babel-load-languages (quote ((python . t) (emacs-lisp . t))))
  '(org-modules
