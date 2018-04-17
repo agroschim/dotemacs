d9f3e9f9ff9915dac03e27c8969c8f97e6c2f78f
agroschim <agroschim@gmx.de>
2018-04-17 19:42:56 +0200

    Set default input method

--- custom.el
+++ custom.el
@@ -3,6 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(default-input-method "cyrillic-translit")
  '(inhibit-startup-screen t)
  '(org-babel-load-languages (quote ((python . t) (emacs-lisp . t))))
  '(org-modules
