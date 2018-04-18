3da6c338c123fd6ffb813d67eaade2bc54db7474
agroschim <agroschim@gmx.de>
2018-04-18 18:26:48 +0200

    Set custom font

--- custom.el
+++ custom.el
@@ -17,4 +17,5 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(default ((t (:inherit nil :stipple nil :background "#272822" :foreground "#F8F8F2" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 120 :width normal :foundry "1ASC" :family "Droid Sans Mono"))))
  '(all-the-icons-dired-dir-face ((t (:foreground "#66D9EF")))))
