6f3541a11265702c14808c4298426143c1717708
agroschim <agroschim@gmx.de>
2019-01-28 19:30:04 +0100

    Installed csound-mode

--- custom.el
+++ custom.el
@@ -11,7 +11,7 @@
     (org-bbdb org-bibtex org-docview org-gnus org-info org-irc org-mhe org-rmail org-w3m)))
  '(package-selected-packages
    (quote
-    (pdf-tools ereader chess ox-twbs ztree golden-ratio all-the-icons all-the-icons-dired cider magit diff-hl hy-mode auto-complete yasnippet tree-mode rainbow-mode powerline ox-mediawiki monokai-theme json-mode iedit flycheck expand-region dired-subtree csharp-mode auto-complete-c-headers auto-complete-auctex))))
+    (csound-mode pdf-tools ereader ox-twbs ztree golden-ratio all-the-icons all-the-icons-dired magit diff-hl hy-mode auto-complete yasnippet tree-mode rainbow-mode powerline ox-mediawiki monokai-theme json-mode iedit flycheck expand-region dired-subtree csharp-mode auto-complete-c-headers auto-complete-auctex))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
