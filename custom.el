8f2a7ccdf5742e2018d8e7f2b7f734b036ab878c
agroschim <agroschim@gmx.de>
2022-02-02 10:29:59 +0100

    Packages updated and =linum-mode= replaced

--- custom.el
+++ custom.el
@@ -13,7 +13,7 @@
  '(org-modules
    '(org-tempo org-bbdb org-bibtex org-docview org-gnus org-info org-irc org-mhe org-rmail org-w3m))
  '(package-selected-packages
-   '(all-the-icons-dired alert pomidor htmlize graphviz-dot-mode web-beautify csound-mode pdf-tools ereader ox-twbs ztree magit diff-hl hy-mode auto-complete yasnippet tree-mode rainbow-mode powerline ox-mediawiki monokai-theme json-mode iedit flycheck expand-region dired-subtree csharp-mode auto-complete-c-headers auto-complete-auctex))
+   '(all-the-icons-dired pomidor htmlize graphviz-dot-mode web-beautify csound-mode pdf-tools ereader ox-twbs ztree magit diff-hl hy-mode auto-complete yasnippet tree-mode rainbow-mode powerline ox-mediawiki monokai-theme json-mode iedit flycheck expand-region dired-subtree csharp-mode auto-complete-c-headers auto-complete-auctex))
  '(pomidor-alert nil)
  '(pomidor-sound-break-over "")
  '(pomidor-sound-overwork "")
@@ -24,5 +24,5 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "#272822" :foreground "#F8F8F2" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 120 :width normal :foundry "outline" :family "Dejavu Sans Mono"))))
+'(default ((t (:inherit nil :stipple nil :background "#272822" :foreground "#F8F8F2" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 120 :width normal :foundry "outline" :family "Dejavu Sans Mono"))))
  '(all-the-icons-dired-dir-face ((t (:foreground "#66D9EF")))))
